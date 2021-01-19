4b0db95b7f2dfa5a087759efd27dc888d3c5210e
Michael David Lynch <mdl@Michaels-MacBook-Air-3.local>
2021-01-19 07:37:35 -0500

    Release 2

--- config/keys.js
+++ config/keys.js
@@ -3,13 +3,13 @@
 
 module.exports ={
     google:{
-    clientID:'187300875259-lbllv021gd80kobe24rqm015tlefcpv5.apps.googleusercontent.com',
-    clientSecret:'BsD1Mr5JmPhNogvrcOwR4a3c'
+    clientID:' TBD ',
+    clientSecret:'TBD'
     },
     mongodb:{
-        dbURI:'mongodb+srv://mdl:jmpa7137@cluster1.ryjim.gcp.mongodb.net/BioProcessTEACalculator?retryWrites=true&w=majority'
+        dbURI:'TBD'
     },
     session:{
-        cookieKey:'ha845hapvmeb72qe07mafiay62rvvxg'
+        cookieKey:'TBD'
     }
 };
